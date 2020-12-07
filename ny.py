6cb65b09b649bf10eb4684342997660d16b4bb9f
quantum9innovation <46249765+quantum9Innovation@users.noreply.github.com>
2020-12-06 21:23:57 -0800

    Formatted New York Data

---
+++ ny.py
@@ -0,0 +1,15 @@
+import os
+print("Current working directory at: " + os.getcwd())
+
+print("Fetching raw hospitalization data for New York from directory: ")
+print("data/raw/ny-hospitalizations.csv")
+
+ny_hos_data = open("data/raw/ny-hospitalizations.csv", "r").readlines()
+formatted_ny_hos_data = open("data/formatted/ny-hospitalizations.csv", "w+")
+
+for line in range(1, len(ny_hos_data)):
+
+    formatted_ny_hos_data.writelines([ny_hos_data[line].split(',')[2] + '\n'])
+
+print("Raw hospitalization data has been formatted.")
+print("The formatted hospitalization data can be found at `data/formatted/ny-hospitalizations`")
